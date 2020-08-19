29bdc759908d2c04001eab398bf731a7776b4b70
EduardoBaptista01 <e.f.baptista@hotmail.com>
2020-08-19 16:07:36 +0100

    Updated app.py
    Just making it easier for the testing phase, added our id's

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
-
 client = discord.Client()
+#This is to be removed at a later stage, it's just for testing
+sheep = client.get_user(261155597993377792)
+march = client.get_user(141180424964669440)
 
 
 @client.event
@@ -24,7 +26,7 @@ async def on_ready():
 async def on_message(message):
     gordo = client.get_user(int(os.getenv('DISCORD_USER')))
     print(gordo)
-    if message.author == gordo:
+    if message.author == gordo or message.author == sheep or message.author == march:
         await message.channel.purge(limit=1)
 
     if message.content.startswith('$hello'):
