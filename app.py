76c1612cb563b60d3f2ee49777ff01049b4f2a7a
EduardoBaptista01 <e.f.baptista@hotmail.com>
2020-08-20 14:22:41 +0100

    Added role remover

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ async def on_ready():
 #gordo = client.get_user(524276962106146838)
 #print('gordo:', gordo)
 
+#Message Deleter
 @client.event   
 async def on_message(message):
     
@@ -34,7 +35,8 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
-        
+
+#Disconnecter    
 @client.event 
 async def on_voice_state_update(member, before, after):
     now = datetime.now()
@@ -57,5 +59,35 @@ async def on_voice_state_update(member, before, after):
     if member == gordo or member == sheep or member == march:
         await member.edit(voice_channel=None)
     
+#Role remover
+@client.event 
+async def on_member_update(before, after):
+    now = datetime.now()
+    timestamp = datetime.timestamp(now)
     
+    #Stuff to remove later
+    sheep = client.get_user(int(os.getenv('DISCORD_SHEEP')))
+    march = client.get_user(int(os.getenv('DISCORD_MARCH')))
+    gordo = client.get_user(int(os.getenv('DISCORD_USER')))
+    
+    #Print roles in log
+    #print(now, "-", before.roles)
+    #print(now, "-", after.top_role)
+    
+    #This is to check if someone on the hitlist changed roles
+    #"bitch" must be replaced with "Professor Chaos" on the other server
+    if after == gordo or after == sheep or after == march and str(after.top_role) == "bitch":
+        list_roles = after.roles.copy()
+        
+        #This is to check if professor chaos aka bitch is one of the roles
+        for index, x in enumerate(list_roles, start=0):
+            #Getting index of "bitch"
+            if str(x) == "bitch":
+                index_role = index
+                del list_roles[index_role]
+                await after.edit(roles=list_roles)
+                print(after.roles)
+                break
+        
+
 client.run(TOKEN)
