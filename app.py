797ff55b55271d41999fc5b60e8fa36d9c498be0
David Pinto <davidpinto15@gmail.com>
2020-09-23 17:23:12 +0100

    aded new TODO's added comands to mentions people by games

--- app.py
+++ app.py
@@ -31,39 +31,45 @@ client = discord.Client()
 - Criar permissoes nas salas para ele nem sequer conseguir entrar( not sure if this one works)
 - Convinha que ele conseguisse reproduzir musicas do spotify e youtube para nao parecer que é totalmente inutil, provavelmente já há codigo disso por aí, não deve ser muito dificil - TRUE
 - Criar comandos do tipo "annoy [user_id]" para podermos fazer as cenas on the fly
-- Comandos para dar tag a pessoal de vários jogos, like !pummel ou !amongUs etc
+- Comandos para dar tag a pessoal de vários jogos, like !pummel ou !amongUs etc - DONE!
+- meter estado do bot para !help e o help dizer pra perguntar ao sheep ou ao march
+- Criar audit log num file que elimina apos +/- 30 dias
+- Comando pra o mata kickar o bifes
 """
-
-#This is to be removed at a later stage, it's just for testing (basta tirar o sheep e march, o gordo fica ja)
-sheep = client.get_user(int(os.getenv('DISCORD_SHEEP')))
-march = client.get_user(int(os.getenv('DISCORD_MARCH')))
-gordo = client.get_user(int(os.getenv('DISCORD_GORDO')))
-mata = client.get_user(int(os.getenv('DISCORD_MATA')))
-bacon = client.get_user(int(os.getenv('DISCORD_BACON')))
-tiago = client.get_user(int(os.getenv('DISCORD_TIAGO'))) #aka sheeps best friend in the whole world kappa, nao apagues, e pro comando "clash" de lol
-fontes = client.get_user(int(os.getenv('DISCORD_FONTES')))
 
 @client.event   
 async def on_message(message):
+    #Gets the user by its id - Necessary have one of this inside every event cuz if its outside it brakes cuz it happens before the client is created
+    sheep = client.get_user(int(os.getenv('DISCORD_SHEEP')))
+    march = client.get_user(int(os.getenv('DISCORD_MARCH')))
+    gordo = client.get_user(int(os.getenv('DISCORD_GORDO')))
+    mata = client.get_user(int(os.getenv('DISCORD_MATA')))
+    bacon = client.get_user(int(os.getenv('DISCORD_BACON')))
+    tiago = client.get_user(int(os.getenv('DISCORD_TIAGO'))) #aka sheeps best friend in the whole world kappa, nao apagues, e pro comando "clash" de lol
+    fontes = client.get_user(int(os.getenv('DISCORD_FONTES')))
+    #print(sheep, march)
+
     #Message Deleter-------
-    
-    if message.author == gordo or message.author == march:
+    if message.author == gordo:
         print('message author: ', message.author)
         await message.channel.purge(limit=1)
 
-    #Commands to invite people for games -----------------------------------------------------
-    channel = message.channel
-    print('channel:', channel)
+    #Commands to invite people for games -------------------------------------
     if message.content.startswith("!pummel"):
-        print('pummel')
-        #await channel.send('pummel here')
-        #await channel.send('%s is the best ' % march)
-    elif message.content.startswith == "!r6":
-        pass
-    elif message.content.startswith == "!amongus":
-        pass
-    elif message.content.startswith == "!clash":
-        pass
+        print('pummel by: ', message.author)
+        await message.channel.send('Sessão de pummel hoje?\n' + march.mention + ' ' + sheep.mention + ' ' + bacon.mention + ' ' + mata.mention)
+    elif message.content.startswith("!r6"):
+        print('r6 by: ', message.author)
+        await message.channel.send('Alguém quer vir rainbow?\n Sheep Instala o rainbow!\n' + march.mention + ' ' + sheep.mention + ' ' + bacon.mention + ' ' + mata.mention + ' ' + fontes.mention)
+    elif message.content.startswith("!amongus"):
+        print('among us by: ', message.author)
+        await message.channel.send('Sessão de Among Us?\n' + march.mention + ' ' + sheep.mention + ' ' + bacon.mention + ' ' + mata.mention + ' ' + fontes.mention)
+    elif message.content.startswith("!clash"):
+        print('clash by: ', message.author)
+        await message.channel.send('Clash este fds? Alguém não pode?\n' + march.mention + ' ' + tiago.mention + ' ' + bacon.mention + ' ' + mata.mention + ' ' + fontes.mention)
+    elif message.content.startswith("!help"):
+        print('help by: ', message.author)
+        await message.channel.send(march.mention + sheep.mention + '\nEsta aqui um nabo a pedir ajuda...\nPergunta a um destes dois se eles nao responderem!')
 
 
 #Disconnecter    
@@ -73,7 +79,7 @@ async def on_voice_state_update(member, before, after):
     #Stuff to remove later
     sheep = client.get_user(int(os.getenv('DISCORD_SHEEP')))
     march = client.get_user(int(os.getenv('DISCORD_MARCH')))
-    gordo = client.get_user(int(os.getenv('DISCORD_USER')))
+    gordo = client.get_user(int(os.getenv('DISCORD_GORDO')))
     
     #Simple channel movements log 
     if before.channel is None:
@@ -94,7 +100,7 @@ async def on_member_update(before, after):
     #Stuff to remove later
     sheep = client.get_user(int(os.getenv('DISCORD_SHEEP')))
     march = client.get_user(int(os.getenv('DISCORD_MARCH')))
-    gordo = client.get_user(int(os.getenv('DISCORD_USER')))
+    gordo = client.get_user(int(os.getenv('DISCORD_GORDO')))
     
     #This is to check if someone on the hitlist changed roles
     #TODO "bitch" must be replaced with "Professor Chaos" on the other server
