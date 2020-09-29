1347ff86ae47fe3c0812ec9d7c259e7e7949fa2f
EduardoBaptista01 <e.f.baptista@hotmail.com>
2020-09-29 13:37:51 +0100

    Correted !leave emoji

--- app.py
+++ app.py
@@ -465,7 +465,7 @@ class Music(commands.Cog):
         if not ctx.voice_state.voice:
             return await ctx.send('Not connected to any voice channel.')
 
-        await ctx.message.add_reaction(':wave:')
+        await ctx.message.add_reaction('👋')
         await ctx.voice_state.stop()
         del self.voice_states[ctx.guild.id]
 
